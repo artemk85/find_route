73e33fe6baa2ce62030b062b1cda8374fde4bbec
ArtemK <kochetkov1985@gmail.com>
2021-02-27 21:59:49 +0300

    Внесены изменения

--- cities/views.py
+++ cities/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
@@ -13,7 +16,10 @@ def index(request):
 def home(request):
     form = CityForm()
     list_cities = City.objects.all()
-    context = {'objects_list': list_cities, 'form': form}
+    lst = Paginator(list_cities, 2)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj, 'form': form }
     return render(request, 'cities/home.html', context)
 
 
@@ -29,14 +35,30 @@ def add_city(request):
             form.save()
     return redirect('cities:home')
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
     success_url = reverse_lazy('cities:home')
+    success_message = "Город успешно создан."
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
     success_url = reverse_lazy('cities:home')
+    success_message = "Город успешно изменен."
+
+class CityDeleteView(SuccessMessageMixin, DeleteView):
+    model = City
+    #template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Город успешно удален.')
+        return self.post(request, *args, **kwargs)
+
+class CityListView(ListView):
+    paginate_by = 5
+    model = City
+    template_name = 'cities/home.html'
